4663c6e1f17f4f4403f1dbb51ffc181a356799c0
Sean Winnik <winnik.sean@gmail.com>
2023-04-18 16:56:28 -0700

    Complete bare_minimum/promiseConstructor.js exercises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,33 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, ((err, results) => {
+    if (err) {
+      // console.log('fs.readFile failed :(\n', err)
+      callback(err)
+    } else {
+      results = results.toString().slice(0, results.indexOf('\n'))
+      // console.log(results)
+      callback(null, results)
+      //'This is a file to read\nthat once it\'s been read\nwill not turn red.' to equal 'This is a file to read'
+    }
+  })
+  )
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+  console.log("URL:", url)
+  request('get', url, (err, response) => {
+    if (err) {
+      callback(err)
+    } else {
+      callback(null, response.statusCode)
+    }
+  })
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -11,11 +11,31 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, ((err, data)=> {
+      if(err){
+        reject(err)
+      } else {
+        var newData = data.toString('utf8')
+        resolve(newData.slice(0, newData.indexOf('\n')));
+      }
+    }))
+  })
 };
+
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+  return new Promise((resolve, reject) => {
+    request('get', url, (err, response) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(response.statusCode)
+      }
+    })
+  })
 };
 
 // Export these functions so we can test them and reuse them in later exercises
